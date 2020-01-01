4a90c2999c9ce22a1c769e7a65255199f74f1d11
Lone Nilsson <onlylonely1986@gmail.com>
2020-01-01 18:25:53 +0100

    easy starting up application

---
+++ Application.php
@@ -0,0 +1,21 @@
+<?php
+
+// require_once("model/UserStorage.php");
+// require_once("model/ScribbleStorage.php");
+// require_once("model/SessionModel.php");
+// require_once("controller/LoginController.php");
+// require_once("controller/RegisterController.php");
+// require_once("controller/ScribbleController.php");
+// require_once("view/ExceptionHTMLMessages.php");
+// require_once("view/LayoutView.php");
+// require_once("view/LoginView.php");
+// require_once("view/RegisterView.php");
+// require_once("view/DateTimeView.php");
+// require_once("view/ScribbleView.php");
+
+class Application {
+
+    public function run() {
+		echo "Run application";
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "hello world";
+/**
+ * index.php
+ * 
+ * The entry point of the application
+ * 
+ * @author Lone Nilsson, Sanna Gustavsson
+ * @version 1.0
+ * @link https://github.com/onlylonely1986/2dv513A3
+ */
+
+require_once('settings.php');
+require_once('Application.php');
+
+session_start();
+
+$settings = new settings();
+$start= new Application($settings);
+$start->run();
+
