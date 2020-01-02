57219fb30eac3abf2780fa7cfc22515088e31728
Sanna Gustafsson <sg222ws@student.lnu.se>
2020-01-02 15:51:49 +0100

    errors

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
  * @link https://github.com/onlylonely1986/2dv513A3
  */
 
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
 require_once('settings.php');
 require_once('Application.php');
 
