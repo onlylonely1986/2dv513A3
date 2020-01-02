14c836fe27776cbce6434ac42445e6f1eececf1d
Lone Nilsson <onlylonely1986@gmail.com>
2020-01-02 12:51:17 +0100

    commit

--- view/ClientView.php
+++ view/ClientView.php
@@ -6,74 +6,20 @@ namespace view;
 // require_once("model/ScribbleItem.php");
 
 class ClientView {
-  private static $send = 'ScribbleView::send';
-  private static $title = 'ScribbleView::title';
-  private static $text = 'ScribbleView::text';
-  private static $removeBtn = 'ScribbleView::remove';
+  private static $send = 'ClientView::send';
+  private static $title = 'ClientView::title';
+  private static $text = 'ClientView::text';
+  private static $removeBtn = 'ClientView::remove';
   private $message;
-  private $userName;
-  private $collection;
 
   public function __construct() {
     $this->message = "";
   }
 
   public function echoHTML($sessionLoggedin) {
-   
-    return '<h2>What s up today ' . $this->userName . ' ?</h2>
+    return '<h2>Add new client to database:</h2>
                  ' . $this->clientFormHTML(). '
-                 ' . $this->iterateOverScribbles() . '
               ';
-  }
-
-  
-  private function checkValidInput() {
-    if(preg_match('/[^\w -!?@#$%^&*()]/', $_POST[self::$title]) || preg_match('/[^\w -!?@#$%^&*()]/', $_POST[self::$text])) {
-      $this->message = Messages::$invalidCharsInInput;
-      self::$title = strip_tags($_POST[self::$title]);
-      self::$text = strip_tags($_POST[self::$text]);
-    } else {
-      $this->message = Messages::$messagePublished;
-      self::$title = $_POST[self::$title];
-      self::$text = $_POST[self::$text];
-    }
-  }
-
-  public function getTitle() {
-    return self::$title;
-  }
-  
-  public function getText() {
-    return self::$text;
-  }
-
-  public function removeScribble() : bool {
-    // TODO implement this 
-    if (isset($_POST[self::$removeBtn])) {
-      return true;
-    } else return false;
-  }
-  
-  public function setCollection($data) {
-    $this->collection = $data;
-  }
-
-  public function setLoggedinState($user) {
-    $this->userName = $user;
-  }
-
-  private function iterateOverScribbles() {
-    $ret = "";
-    foreach ($this->collection as $item) {
-      $user = $item->user;
-      $title = $item->title;
-      $text = $item->text;
-      $ret .= "<p>Post: <b>$user</b>  says: $title || $text</p>";
-      if($this->userName == $item->user) {
-        $ret .= '<input type="submit" name="' . self::$removeBtn . '" value="Remove"/>';
-      }
-    }
-    return $ret;
   }
 
   private function clientFormHTML() {
